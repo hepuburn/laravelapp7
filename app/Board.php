c79d9639a71d4997650cd59ff77faded10230795
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-15 07:53:09 +0900

    連想配列 修正

--- app/Board.php
+++ app/Board.php
@@ -14,9 +14,15 @@ class Board extends Model
        'message' => 'required'
    );
 
+
    public function getData()
    {
-       return $this->id . ': ' . $this->title;
+       return $this->id . ': ' . $this->title . ' (' . $this->person['name'] . ')';
    }
+
+   public function person()
+{
+   return $this->belongsTo('App\Person');
+}
 
 }
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('person/del', 'PersonController@delete');
 Route::post('person/del', 'PersonController@remove');
 
 Route::get('board', 'BoardController@index');
+
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
 
