eea6ed53dd184b4592b61e56456b6b01da8d64f3
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-15 01:45:02 +0900

    scopeperson6-17

--- app/Person.php
+++ app/Person.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 
 class Person extends Model
@@ -30,11 +31,9 @@ public function scopeAgeLessThan($query, $n)
 protected static function boot()
 {
    parent::boot();
-
-   static::addGlobalScope('age', function (Builder $builder) {
-       $builder->where('age', '>', 20);
-   });
+   static::addGlobalScope(new ScopePerson);
 }
+
 
 
 }
