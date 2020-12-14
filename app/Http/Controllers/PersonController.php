da92ae6b1f02f169e2ad1af5796b087906754409
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-15 01:16:00 +0900

    スコープ6-14

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -12,16 +12,22 @@ class PersonController extends Controller
        $items = Person::all();
        return view('person.index', ['items' => $items]);
    }
-   
+
    public function find(Request $request)
 {
    return view('person.find',['input' => '']);
 }
 
+
 public function search(Request $request)
 {
-   $item = Person::find($request->input);
+   $min = $request->input * 1;
+   $max = $min + 10;
+   $item = Person::ageGreaterThan($min)->ageLessThan($max)->first();
    $param = ['input' => $request->input, 'item' => $item];
    return view('person.find', $param);
 }
+
+
+
 }
