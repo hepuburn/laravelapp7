5647d78b1122f252b07a07728ddb1114211dcebd
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-17 00:32:34 +0900

    ディレクトリ指定　vendor/bin/phpunit tests/Feature/MyTest.php

--- tests/Feature/Hellotest.php
+++ tests/Feature/Hellotest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class Hellotest extends TestCase
 {
@@ -14,17 +15,22 @@ class Hellotest extends TestCase
      *
      * @return void
      */
+    use DatabaseMigrations;
     public function testHello()
-    {
-        $this->assertTrue(true);
- 
-        $arr = [];
-        $this->assertEmpty($arr);
- 
-        $msg = "Hello";
-        $this->assertEquals('Hello', $msg);
- 
-        $n = random_int(0, 100);
-        $this->assertLessThan(100, $n);
-    }
+   {
+       $this->assertTrue(true);
+
+       $response = $this->get('/');
+       $response->assertStatus(404);
+      
+       $response = $this->get('/hello');
+       $response->assertStatus(302);
+      
+       $user = factory(User::class)->create();
+       $response = $this->actingAs($user)->get('/hello');
+       $response->assertStatus(200);
+
+       $response = $this->get('/no_route');
+       $response->assertStatus(404);
+   }
 }
