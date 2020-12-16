9dee6d73b12fa5bdc8c95a295b55efc2c6c36faa
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-17 00:40:53 +0900

    データベーステスト7-43

--- tests/Feature/Hellotest.php
+++ tests/Feature/Hellotest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
+use App\Person;
 
 class Hellotest extends TestCase
 {
@@ -16,21 +17,35 @@ class Hellotest extends TestCase
      * @return void
      */
     use DatabaseMigrations;
+
     public function testHello()
-   {
-       $this->assertTrue(true);
-
-       $response = $this->get('/');
-       $response->assertStatus(404);
-      
-       $response = $this->get('/hello');
-       $response->assertStatus(302);
-      
-       $user = factory(User::class)->create();
-       $response = $this->actingAs($user)->get('/hello');
-       $response->assertStatus(200);
-
-       $response = $this->get('/no_route');
-       $response->assertStatus(404);
-   }
+    {
+        // ダミーで利用するデータ
+        factory(User::class)->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+        factory(User::class, 10)->create();
+ 
+        $this->assertDatabaseHas('users', [
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+ 
+        // ダミーで利用するデータ
+        factory(Person::class)->create([
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
+        factory(Person::class, 10)->create();
+ 
+        $this->assertDatabaseHas('people', [
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
+}
 }
